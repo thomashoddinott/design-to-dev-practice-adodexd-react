9e0256ffbf5f081c99291de0b47f274c68f4b0fb
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-12-24 12:24:58 +0000

    wip - v1: absolute positioning; can't resize/zoom in browser. Fix this in v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
-import Bg from './components/Bg'
-
-//struggling to get this background image working...
 
 function App() {
   return (
     <>
-      <Bg>
-    adsadsda
-        </Bg>
       <Header />
       <Hero />
       <Footer />
